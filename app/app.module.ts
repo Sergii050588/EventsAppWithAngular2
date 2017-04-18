864936dd247651c6d67b39c3353218855ce01a0a
Sergii050588 <mozgoved88@mail.ru>
2017-04-18 20:34:04 +0300

    Added 404 page for wron id of event

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core'
+import {Component, NgModule} from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from "@angular/router"
 import { EventsAppComponent } from "./events-app.component";
@@ -10,6 +10,9 @@ import { ToastrService } from "./common/toastr.service";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
 
 import { appRoutes } from "./routes";
+import {CreateEventComponent} from "./events/create-event.component";
+import {Error404Component} from "./errors/404.component";
+import {EventRouteActivator} from "./events/event-details/event-route-activator.service";
 
 @NgModule({
     imports: [
@@ -21,11 +24,18 @@ import { appRoutes } from "./routes";
         EventsListComponent,
         EventThumbnailComponent,
         NavBarComponent,
-        EventDetailsComponent
+        EventDetailsComponent,
+        CreateEventComponent,
+        Error404Component
     ],
     providers: [
-      EventService,
-        ToastrService
+        EventService,
+        ToastrService,
+        EventRouteActivator,
+        {
+            provide: 'canDeactivateCreateEvent',
+            useValue: checkDirtyState
+        }
     ],
     bootstrap: [
         EventsAppComponent
