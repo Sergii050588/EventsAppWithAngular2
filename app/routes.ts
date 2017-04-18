cdd81aa49b785972f9509198fbfcf7f8392bc3e5
Sergii050588 <mozgoved88@mail.ru>
2017-04-18 22:00:27 +0300

    New Module for user added with lazy loading

--- app/routes.ts
+++ app/routes.ts
@@ -15,5 +15,6 @@ export const appRoutes:Routes = [
                           canActivate: [EventRouteActivator] },
     { path: '404',        component: Error404Component },
     { path: '',           redirectTo: '/events',
-                          pathMatch: 'full' }
+                          pathMatch: 'full' },
+    { path: 'user',       loadChildren: 'app/user/user.module#UserModule' }
 ]
