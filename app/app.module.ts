67f2f5ab5e6e253e202943ba23db455d6b869dde
Sergii050588 <mozgoved88@mail.ru>
2017-05-05 15:01:39 +0300

    User can add new Event

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,11 +17,14 @@ import { appRoutes } from "./routes";
 import { Error404Component } from "./errors/404.component";
 import { EventsAppComponent } from "./events-app.component";
 import {AuthService} from "./user/auth.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
     imports: [
         BrowserModule,
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(appRoutes),
+        FormsModule,
+        ReactiveFormsModule
     ],
     declarations: [
         EventsAppComponent,
